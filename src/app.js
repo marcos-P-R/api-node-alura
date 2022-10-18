361bba770d6f7c11406fb2d7039d019fcfc38733
marcos-P-R <marcos.p.mcruz@gmail.com>
2022-10-18 06:59:20 -0300

    adicionando rota
    Adicionado rota para cadastrar novo livro

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 const app = express();
 
+app.use(express.json());
+
 const livros = [
   {"id": 1, "nome": "O Pequeno Principe", "Autor": "Antoine de Saint-Exupéry"},
   {"id": 2, "nome": "O Livro do Chá", "Autor": "Okakura Kakuzō"},
@@ -13,6 +15,11 @@ app.get('/', (req, res) => {
 
 app.get('/livros', (req, res) => {
   res.status(200).json({"livros" : livros});
+});
+
+app.post('/livros', (req, res) => {
+  livros.push(req.body);
+  res.status(201).send('livro cadastrado');
 });
 
 export default app;
