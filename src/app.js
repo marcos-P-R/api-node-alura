bf0c43c9b75e11c62a7e2af5b5620e8cf4a32804
marcos-P-R <marcos.p.mcruz@gmail.com>
2022-11-16 07:20:24 -0300

    Add mongoose
    Adicionando biblioteca para conectar com o mongodb

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
 import express from "express";
+import db from "./config/dbConnect.js"
+
+db.on("erro", console.log.bind(console, "Erro de conexão"));
+db.once("open", () => {
+  console.log("Conexão com o banco feita com sucesso");
+});
+
 const app = express();
 
 app.use(express.json());
