c47f6a5393d6b45062723acbddd338a60d71405f
marcos-P-R <marcos.p.mcruz@gmail.com>
2022-11-16 06:43:27 -0300

    Add rotas
    Adicionando rotas de operações CRUD da api

--- src/app.js
+++ src/app.js
@@ -17,9 +17,36 @@ app.get('/livros', (req, res) => {
   res.status(200).json({"livros" : livros});
 });
 
+app.get('/livros/:id', (req, res) => {
+  const id = req.params.id;
+  const index = buscarLivro(id)
+  const nome = req.body.nome
+  livros[index].nome = nome;
+  res.status(200).json(livros[index]);
+});
+
 app.post('/livros', (req, res) => {
   livros.push(req.body);
   res.status(201).send('livro cadastrado');
 });
+
+app.put('/livros/:id', (req, res) => {
+  const id = req.params.id;
+  const index = buscarLivro(id)
+  const nome = req.body.nome
+  livros[index].nome = nome;
+  res.status(200).json({livros});
+});
+
+app.delete('/livros/:id', (req, res) => {
+  const {id} = req.params;
+  const index = buscarLivro(id)
+  livros.splice(index, 1)
+  res.status(200).json({messagem: `Livro ${id} excluido`});
+});
+
+function buscarLivro(id) {
+  return livros.findIndex(livro => livro.id == id);
+}
 
 export default app;
