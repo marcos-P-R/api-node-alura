c7fc214dff6cca791d02ec5aed6f6f8690687de4
marcos-P-R <marcos.p.mcruz@gmail.com>
2022-11-17 19:42:06 -0300

    Criando schema com o mongo
    criando schema para criar e adicionar um livro

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
-import db from "./config/dbConnect.js"
+import db from "./config/dbConnect.js";
+import livros from "./model/Livro.js";
 
 db.on("erro", console.log.bind(console, "Erro de conexão"));
 db.once("open", () => {
@@ -10,18 +11,24 @@ const app = express();
 
 app.use(express.json());
 
-const livros = [
-  {"id": 1, "nome": "O Pequeno Principe", "Autor": "Antoine de Saint-Exupéry"},
-  {"id": 2, "nome": "O Livro do Chá", "Autor": "Okakura Kakuzō"},
-  {"id": 3, "nome": "O Louvor das Sombras","Autor": "Junichiro Tanizaki"}
-];
+// const livros = [
+//   {"id": 1, "nome": "O Pequeno Principe", "Autor": "Antoine de Saint-Exupéry"},
+//   {"id": 2, "nome": "O Livro do Chá", "Autor": "Okakura Kakuzō"},
+//   {"id": 3, "nome": "O Louvor das Sombras","Autor": "Junichiro Tanizaki"}
+// ];
 
 app.get('/', (req, res) => {
   res.status(200).send('curso de node');
 });
 
 app.get('/livros', (req, res) => {
-  res.status(200).json({"livros" : livros});
+  livros.find((err, livros) => {
+    if(err) {
+      console.log(err)
+    }
+    console.log(livros)
+    res.status(200).json({livros});
+  })
 });
 
 app.get('/livros/:id', (req, res) => {
